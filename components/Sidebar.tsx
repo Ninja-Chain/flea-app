dfe0bec064052d834bc83ec01d0736a7fe156263
peaceandwhisky <guitarfullife@gmail.com>
2021-12-04 19:35:36 +0900

    createページを追加

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -62,7 +62,7 @@ const Sidebar = (): ReactElement => {
           </li>
           <li className="my-5">
             <a
-              href=""
+              href="/create"
               className="flex flex-row items-center h-10 px-3 rounded-lg text-gray-400 hover:bg-gray-100 hover:text-gray-700"
             >
               <span className="flex items-center justify-center text-lg text-gray-400">
@@ -105,30 +105,6 @@ const Sidebar = (): ReactElement => {
                 </svg>
               </span>
               <span className="ml-3">Wrapped CONST</span>
-            </a>
-          </li>
-          <li className="my-5">
-            <a
-              href=""
-              className="flex flex-row items-center h-10 px-3 rounded-lg text-gray-400 hover:bg-gray-100 hover:text-gray-700"
-            >
-              <span className="flex items-center justify-center text-lg text-gray-400">
-                <svg
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  className="h-6 w-6"
-                >
-                  <path
-                    d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-                  />
-                  <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                </svg>
-              </span>
-              <span className="ml-3">CONFIG</span>
             </a>
           </li>
         </ul>
