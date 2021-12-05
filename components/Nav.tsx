b37dc957a2362f1fcdc72f9dc0e1833947cc1a35
tadashim <ma.tadashi@gmail.com>
2021-12-05 17:22:01 +0900

    アイテム一覧画面のデザイン

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -14,7 +14,7 @@ function Nav() {
   }
 
   return (
-    <div className="border-b w-screen px-2 md:px-16 bg-yellow-400">
+    <div className="border-b w-full px-2 md:px-16 bg-yellow-400">
       <nav className="flex flex-wrap text-center md:text-right md:flex flex-row w-full justify-between items-center py-4 ">
         <div className="flex flex-grow lg:flex-grow-0 max-w-full">
           <button
