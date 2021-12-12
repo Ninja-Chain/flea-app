d120312762d91599abc09bcc155672ab1eca4921
peaceandwhisky <guitarfullife@gmail.com>
2021-12-12 18:23:07 +0900

    add images domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['tailwindui.com', 'dummyimage.com', 'drive.google.com']
+    domains: ['tailwindui.com', 'dummyimage.com', 'drive.google.com', 'doc-0g-a8-docs.googleusercontent.com', 'ipfs.io', 'i.imgur.com', 'uanon.observer']
   }
 }
